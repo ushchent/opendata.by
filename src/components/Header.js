8fc83740e9bd9ce5f2cb0845fa4972953f207001
alexey medvetsky <am@opendata.by>
2025-01-04 09:22:13 +0300

    Загрузка

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+	return <header>
+				<h1>Хорошие данные</h1>
+				<p className="caption">для людей и машин</p>
+				<p>Многовекторная оценка обстановки в Беларуси</p>
+			</header>
+}
+
+export default Header;
